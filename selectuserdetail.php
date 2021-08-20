f4bcbca80a462188ad1b669d74578bc3e2774890
abhilashyasangi <84038830+abhilashyasangi@users.noreply.github.com>
2021-08-20 18:39:59 +0530

    Create selectuserdetail.php

---
+++ selectuserdetail.php
@@ -0,0 +1,26 @@
+<?php
+include 'config.php';
+
+if(isset($_POST['submit']))
+{
+    $from = $_GET['id'];
+    $to = $_POST['to'];
+    $amount = $_POST['amount'];
+
+    $sql = "SELECT * from users where id=$from";
+    $query = mysqli_query($conn,$sql);
+    $sql1 = mysqli_fetch_array($query); 
+
+    $sql = "SELECT * from users where id=$to";
+    $query = mysqli_query($conn,$sql);
+    $sql2 = mysqli_fetch_array($query);
+
+
+
+    
+    if (($amount)<0)
+   {
+        echo '<script type="text/javascript">';
+        echo ' alert("Oops! Negative values cannot be transferred")'; 
+        echo '</script>';
+    }
